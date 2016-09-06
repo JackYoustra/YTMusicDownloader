a6d92291e45446f09eab5ecf8234e7ad8374429a
Jack Youstra <jack@youstra.com>
2016-09-05 22:34:50 -0700

    Made changes

--- src/Client.py
+++ src/Client.py
@@ -7,5 +7,7 @@ import FileDownloader
 
 if __name__ == '__main__':
     dubsteppath = "https://www.youtube.com/watch?v=fWRISvgAygU"
+    dubsteppattern = "ARTIST - TITLE HH:MM:SS"
     catalystpath = "https://www.youtube.com/watch?v=PNjqP9JLN-c"
+    catalystpattern = ""
     FileDownloader.download(catalystpath)
